b772e47d3bf62dd1210d8461b7739f3fe981d452
Mohamed Adel <m7md20051968@gmail.com>
2026-09-02 11:15:01 +0300

    Delete core directory

--- core/ops/s3/__init__.py
+++
@@ -1,6 +0,0 @@
-from .io import IoOps
-from .bucket import BucketOps
-from .object import ObjectOps
-from .analytics import AnalyticsOps
-
-__all__ = ["IoOps", "BucketOps", "ObjectOps", "AnalyticsOps"]
